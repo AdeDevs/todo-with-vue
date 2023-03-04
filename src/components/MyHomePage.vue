a788405c0206689ab0d33849c93eab6abacefe79
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-03-04 14:15:04 +0100

    Updated

--- src/components/MyHomePage.vue
+++ src/components/MyHomePage.vue
@@ -71,6 +71,10 @@
         <p>A to-do-list app built using VueJS which implements the CRUD functionalities and uses a JSON server as a backend locally. <a id="special" href="https://github.com/AdeDevs/TaskTracker" target="_blank">Source Code</a></p>
       </section>
       <section>
+        <h1><a href="https://kryptykhamburger.netlify.app/" target="_blank">hamburger menu</a></h1>
+        <p>A simple webpage of a responsive hamburger menu(this one is a personal favourite because I had been putting learning and building it off for so long).</p>
+      </section>
+      <section>
         <h1><a href="https://kryptykcalculator.web.app/" target="_blank">calculator</a></h1>
         <p>A simple calculator singluar webpage built with ReactJS. Completes simple operations</p>
       </section>
